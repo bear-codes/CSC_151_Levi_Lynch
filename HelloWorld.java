ee582bbf3ed4a8004c961266639e915ae3d62217
SANDHILLS\LH218-16 <jad.talbert@gmail.com>
2026-01-21 11:19:15 -0500

    Test Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,15 @@
+/*
+Author: Levi Lynch
+Date: 1/21/26
+@purpose: My First Java Program 
+
+note: This program should reside in the directory pat has follows-> CSC_151_LeviLynch
+*/
+
+public class HelloWorld{
+    public static void main (String[] args)
+    {
+        System.out.println("Hellow World");
+    }
+}
 
